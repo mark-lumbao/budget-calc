6f4b733a71dea4521efaa1bdecb9937cc23c6f5d
Mark Anthony Lumbao <markanthonylumbao@gmail.com>
2021-05-22 16:08:28 +0800

    fix: Fix 1 day deficit in get_days_till_next_salary function

--- src/util.rs
+++ src/util.rs
@@ -2,12 +2,15 @@ use chrono::prelude::*;
 use chrono::Duration;
 
 pub fn get_days_till_next_salary() -> u32 {
-  let mut count: u32 = 0;
+  let mut count: u32 = 1; // @note start with 1 to include today
   let days_till_next_salary = loop {
     let dayoffset = Local::today() + Duration::days(count.into());
-
-    if dayoffset.day() == 5 || dayoffset.day() == 20 { // temporary values 5 and 20
-      break count + 1; // @note include the day today
+    println!("{}, {}, {}", count, Local::today().day(), dayoffset.day());
+    /* @todo
+     * make salary dates dynamic by making including it in the arguments
+     */
+    if dayoffset.day() == 5 || dayoffset.day() == 20 {
+      break count + 1;
     } else { count += 1; }
   };
   days_till_next_salary
